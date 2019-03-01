8d3ad6d097eeb4be1f8a110b1891f05e8fe2fef6
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-03-01 02:06:24 -0500

    Fix runtime errors

--- test/betweenness_centrality_test.cpp
+++ test/betweenness_centrality_test.cpp
@@ -182,15 +182,8 @@ run_unweighted_test(Graph*, std::size_t V, unweighted_edge edge_init[],
     BOOST_TEST(centrality[v] == centrality2[v]);
 
     double relative_error = 
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && \
-    defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-    (7 == __clang_major__)
       (correct_centrality[v] == 0.0) ? centrality[v]
       : ((centrality[v] - correct_centrality[v]) / correct_centrality[v]);
-#else
-      correct_centrality[v] == 0.0? centrality[v]
-      : (centrality[v] - correct_centrality[v]) / correct_centrality[v];
-#endif
     if (relative_error < 0) relative_error = -relative_error;
     BOOST_TEST(relative_error < error_tolerance);
   }  
@@ -201,17 +194,9 @@ run_unweighted_test(Graph*, std::size_t V, unweighted_edge edge_init[],
 
     if (correct_edge_centrality) {
       double relative_error = 
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && \
-    defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-    (7 == __clang_major__)
         (correct_edge_centrality[e] == 0.0) ? edge_centrality1[e]
         : ((edge_centrality1[e] - correct_edge_centrality[e]) 
         / correct_edge_centrality[e]);
-#else
-        correct_edge_centrality[e] == 0.0? edge_centrality1[e]
-        : (edge_centrality1[e] - correct_edge_centrality[e]) 
-        / correct_edge_centrality[e];
-#endif
       if (relative_error < 0) relative_error = -relative_error;
       BOOST_TEST(relative_error < error_tolerance);
 
@@ -477,15 +462,8 @@ void random_unweighted_test(Graph*, std::size_t n)
                   centrality2.begin())) {
     for (std::size_t v = 0; v < centrality.size(); ++v) {
       double relative_error = 
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && \
-    defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-    (7 == __clang_major__)
         (centrality[v] == 0.0) ? centrality2[v]
         : ((centrality2[v] - centrality[v]) / centrality[v]);
-#else
-        centrality[v] == 0.0? centrality2[v]
-        : (centrality2[v] - centrality[v]) / centrality[v];
-#endif
       if (relative_error < 0) relative_error = -relative_error;
       BOOST_TEST(relative_error < error_tolerance);
     }
@@ -518,15 +496,8 @@ void random_unweighted_test(Graph*, std::size_t n)
                   centrality3.begin())) {
     for (std::size_t v = 0; v < centrality.size(); ++v) {
       double relative_error = 
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && \
-    defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-    (7 == __clang_major__)
         (centrality[v] == 0.0) ? centrality3[v]
         : ((centrality3[v] - centrality[v]) / centrality[v]);
-#else
-        centrality[v] == 0.0? centrality3[v]
-        : (centrality3[v] - centrality[v]) / centrality[v];
-#endif
       if (relative_error < 0) relative_error = -relative_error;
       BOOST_TEST(relative_error < error_tolerance);
     }
