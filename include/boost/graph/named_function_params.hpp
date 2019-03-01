406653d3044f6b21e844894dde68ca2ab0f1e9d7
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-03-01 15:12:25 -0500

    Fix runtime errors

--- include/boost/graph/named_function_params.hpp
+++ include/boost/graph/named_function_params.hpp
@@ -556,19 +556,7 @@ BOOST_BGL_ONE_PARAM_CREF(visitor, graph_visitor)
     template <typename ArgType, typename Prop, typename Graph, bool Exists>
     struct override_property_t {
       typedef ArgType result_type;
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && ( \
-        ( \
-            !defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && ( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) || ( \
-            defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && !( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) \
-    )
+#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS)
       result_type operator()(Graph&, typename boost::add_lvalue_reference<ArgType>::type a) const {return a;}
 #else
       result_type operator()(const Graph&, typename boost::add_lvalue_reference<ArgType>::type a) const {return a;}
@@ -578,19 +566,7 @@ BOOST_BGL_ONE_PARAM_CREF(visitor, graph_visitor)
     template <typename ArgType, typename Prop, typename Graph>
     struct override_property_t<ArgType, Prop, Graph, false> {
       typedef typename boost::property_map<Graph, Prop>::type result_type;
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && ( \
-        ( \
-            !defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && ( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) || ( \
-            defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && !( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) \
-    )
+#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS)
       result_type operator()(Graph& g, const ArgType&) const {return get(Prop(), g);}
 #else
       result_type operator()(const Graph& g, const ArgType&) const {return get(Prop(), g);}
@@ -612,19 +588,7 @@ BOOST_BGL_ONE_PARAM_CREF(visitor, graph_visitor)
 
     template <typename ArgPack, typename Tag, typename Prop, typename Graph>
     typename override_property_result<ArgPack, Tag, Prop, Graph>::type
-#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS) && ( \
-        ( \
-            !defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && ( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) || ( \
-            defined(BOOST_PARAMETER_HAS_PERFECT_FORWARDING) && !( \
-                defined(BOOST_CLANG) && defined(__APPLE_CC__) && \
-                (__clang_major__ < 9) \
-            ) \
-        ) \
-    )
+#if defined(BOOST_GRAPH_CONFIG_CAN_NAME_ARGUMENTS)
     override_property(const ArgPack& ap, const boost::parameter::keyword<Tag>& t, Graph& g, Prop)
 #else
     override_property(const ArgPack& ap, const boost::parameter::keyword<Tag>& t, const Graph& g, Prop)
