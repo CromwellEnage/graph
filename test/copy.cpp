4b98678f30397307923d8aa811885d8de9172659
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-04-26 12:16:20 -0400

    Mark compiler failures due to running out of virtual memory as expected

--- test/copy.cpp
+++ test/copy.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) Vladimir Prus 2003.
 // Copyright (C) Cromwell D. Enage 2019.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/graph/adjacency_list.hpp>
