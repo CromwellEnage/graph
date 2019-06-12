dd02f3c3c96b72994476b4e68a1ad3ec7af20c7e
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2019-06-12 06:05:37 -0400

    Upgrade documentation of Boost.Parameter-enabled algorithms

--- include/boost/graph/dijkstra_shortest_paths_no_color_map.hpp
+++ include/boost/graph/dijkstra_shortest_paths_no_color_map.hpp
@@ -19,156 +19,176 @@
 
 namespace boost { namespace graph {
 
-  // No init version
-  template <typename Graph, typename DijkstraVisitor,
-            typename PredecessorMap, typename DistanceMap,
-            typename WeightMap, typename VertexIndexMap,
-            typename DistanceCompare, typename DistanceWeightCombine,
-            typename DistanceInfinity, typename DistanceZero>
-  void dijkstra_shortest_paths_no_color_map_no_init
-    (const Graph& graph,
-     typename graph_traits<Graph>::vertex_descriptor start_vertex,
-     PredecessorMap predecessor_map,
-     DistanceMap distance_map,
-     WeightMap weight_map,
-     VertexIndexMap index_map,
-     DistanceCompare distance_compare,
-     DistanceWeightCombine distance_weight_combine,
-     DistanceInfinity distance_infinity,
-     DistanceZero distance_zero,
-     DijkstraVisitor visitor)
-  {
-    typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
-    typedef typename property_traits<DistanceMap>::value_type Distance;
+    // No init version
+    template <
+        typename Graph, typename PredecessorMap, typename DistanceMap,
+        typename WeightMap, typename VertexIndexMap, typename DistanceCompare,
+        typename DistanceWeightCombine, typename DistanceInfinity,
+        typename DistanceZero, typename DijkstraVisitor
+    >
+    void dijkstra_shortest_paths_no_color_map_no_init(
+        const Graph& graph,
+        typename graph_traits<Graph>::vertex_descriptor start_vertex,
+        PredecessorMap predecessor_map, DistanceMap distance_map,
+        WeightMap weight_map, VertexIndexMap index_map,
+        DistanceCompare distance_compare,
+        DistanceWeightCombine distance_weight_combine,
+        DistanceInfinity distance_infinity, DistanceZero distance_zero,
+        DijkstraVisitor visitor
+    )
+    {
+        typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+        typedef typename property_traits<DistanceMap>::value_type Distance;
 
-    typedef indirect_cmp<DistanceMap, DistanceCompare> DistanceIndirectCompare;
-    DistanceIndirectCompare
-      distance_indirect_compare(distance_map, distance_compare);
+        typedef indirect_cmp<
+            DistanceMap, DistanceCompare
+        > DistanceIndirectCompare;
+        DistanceIndirectCompare distance_indirect_compare(
+            distance_map, distance_compare
+        );
 
+        // Default - use d-ary heap (d = 4)
+        typedef boost::detail::vertex_property_map_generator<
+            Graph, VertexIndexMap, std::size_t
+        > IndexInHeapMapHelper;
+        typedef typename IndexInHeapMapHelper::type IndexInHeapMap;
+        typedef d_ary_heap_indirect<
+            Vertex, 4, IndexInHeapMap, DistanceMap, DistanceCompare
+        > VertexQueue;
 
-    // Default - use d-ary heap (d = 4)
-    typedef
-      boost::detail::vertex_property_map_generator<Graph, VertexIndexMap, std::size_t>
-      IndexInHeapMapHelper;
-    typedef typename IndexInHeapMapHelper::type IndexInHeapMap;
-    typedef
-      d_ary_heap_indirect<Vertex, 4, IndexInHeapMap, DistanceMap, DistanceCompare>
-      VertexQueue;
+        boost::scoped_array<std::size_t> index_in_heap_map_holder;
+        IndexInHeapMap index_in_heap = IndexInHeapMapHelper(
+            graph, index_map, index_in_heap_map_holder
+        )();
+        VertexQueue vertex_queue(
+            distance_map, index_in_heap, distance_compare
+        );
 
-    boost::scoped_array<std::size_t> index_in_heap_map_holder;
-    IndexInHeapMap index_in_heap =
-      IndexInHeapMapHelper(graph, index_map, index_in_heap_map_holder)();
-    VertexQueue vertex_queue(distance_map, index_in_heap, distance_compare);
+        // Add vertex to the queue
+        vertex_queue.push(start_vertex);
 
-    // Add vertex to the queue
-    vertex_queue.push(start_vertex);
+        // Starting vertex will always be the first discovered vertex
+        visitor.discover_vertex(start_vertex, graph);
 
-    // Starting vertex will always be the first discovered vertex
-    visitor.discover_vertex(start_vertex, graph);
+        while (!vertex_queue.empty())
+        {
+            Vertex min_vertex = vertex_queue.top();
+            vertex_queue.pop();
 
-    while (!vertex_queue.empty()) {
-      Vertex min_vertex = vertex_queue.top();
-      vertex_queue.pop();
+            visitor.examine_vertex(min_vertex, graph);
 
-      visitor.examine_vertex(min_vertex, graph);
+            // Check if any other vertices can be reached
+            Distance min_vertex_distance = get(distance_map, min_vertex);
 
-      // Check if any other vertices can be reached
-      Distance min_vertex_distance = get(distance_map, min_vertex);
+            if (!distance_compare(min_vertex_distance, distance_infinity))
+            {
+                // This is the minimum vertex,
+                // so all other vertices are unreachable
+                return;
+            }
 
-      if (!distance_compare(min_vertex_distance, distance_infinity)) {
-        // This is the minimum vertex, so all other vertices are unreachable
-        return;
-      }
+            // Examine neighbors of min_vertex
+            BGL_FORALL_OUTEDGES_T(min_vertex, current_edge, graph, Graph)
+            {
+                visitor.examine_edge(current_edge, graph);
 
-      // Examine neighbors of min_vertex
-      BGL_FORALL_OUTEDGES_T(min_vertex, current_edge, graph, Graph) {
-        visitor.examine_edge(current_edge, graph);
+                // Check if the edge has a negative weight
+                if (
+                    distance_compare(
+                        get(weight_map, current_edge), distance_zero
+                    )
+                )
+                {
+                    boost::throw_exception(negative_edge());
+                }
 
-        // Check if the edge has a negative weight
-        if (distance_compare(get(weight_map, current_edge), distance_zero)) {
-          boost::throw_exception(negative_edge());
-        }
+                // Extract the neighboring vertex and get its distance
+                Vertex neighbor_vertex = target(current_edge, graph);
+                Distance neighbor_vertex_distance = get(
+                    distance_map, neighbor_vertex
+                );
+                bool is_neighbor_undiscovered = !distance_compare(
+                    neighbor_vertex_distance, distance_infinity
+                );
 
-        // Extract the neighboring vertex and get its distance
-        Vertex neighbor_vertex = target(current_edge, graph);
-        Distance neighbor_vertex_distance = get(distance_map, neighbor_vertex);
-        bool is_neighbor_undiscovered =
-          !distance_compare(neighbor_vertex_distance, distance_infinity);
+                // Attempt to relax the edge
+                bool was_edge_relaxed = relax_target(
+                    current_edge, graph, weight_map, predecessor_map,
+                    distance_map, distance_weight_combine, distance_compare
+                );
 
-        // Attempt to relax the edge
-        bool was_edge_relaxed = relax_target(current_edge, graph, weight_map,
-          predecessor_map, distance_map,
-          distance_weight_combine, distance_compare);
+                if (was_edge_relaxed)
+                {
+                    visitor.edge_relaxed(current_edge, graph);
 
-        if (was_edge_relaxed) {
-          visitor.edge_relaxed(current_edge, graph);
-          if (is_neighbor_undiscovered) {
-            visitor.discover_vertex(neighbor_vertex, graph);
-            vertex_queue.push(neighbor_vertex);
-          } else {
-            vertex_queue.update(neighbor_vertex);
-          }
-        } else {
-          visitor.edge_not_relaxed(current_edge, graph);
-        }
+                    if (is_neighbor_undiscovered)
+                    {
+                        visitor.discover_vertex(neighbor_vertex, graph);
+                        vertex_queue.push(neighbor_vertex);
+                    }
+                    else
+                    {
+                        vertex_queue.update(neighbor_vertex);
+                    }
+                }
+                else
+                {
+                    visitor.edge_not_relaxed(current_edge, graph);
+                }
+            } // end out edge iteration
 
-      } // end out edge iteration
-
-      visitor.finish_vertex(min_vertex, graph);
-    } // end while queue not empty
-  }
-
-  // Full init version
-  template <typename Graph, typename DijkstraVisitor,
-            typename PredecessorMap, typename DistanceMap,
-            typename WeightMap, typename VertexIndexMap,
-            typename DistanceCompare, typename DistanceWeightCombine,
-            typename DistanceInfinity, typename DistanceZero>
-  void dijkstra_shortest_paths_no_color_map
-    (const Graph& graph,
-     typename graph_traits<Graph>::vertex_descriptor start_vertex,
-     PredecessorMap predecessor_map,
-     DistanceMap distance_map,
-     WeightMap weight_map,
-     VertexIndexMap index_map,
-     DistanceCompare distance_compare,
-     DistanceWeightCombine distance_weight_combine,
-     DistanceInfinity distance_infinity,
-     DistanceZero distance_zero,
-     DijkstraVisitor visitor,
-     typename boost::disable_if<
-       parameter::are_tagged_arguments<
-         PredecessorMap, DistanceMap, WeightMap, VertexIndexMap,
-         DistanceCompare, DistanceWeightCombine, DistanceInfinity,
-         DistanceZero, DijkstraVisitor
-       >,
-       mpl::true_
-     >::type = mpl::true_()
-     )
-  {
-    // Initialize vertices
-    BGL_FORALL_VERTICES_T(current_vertex, graph, Graph) {
-      visitor.initialize_vertex(current_vertex, graph);
-
-      // Default all distances to infinity
-      put(distance_map, current_vertex, distance_infinity);
-
-      // Default all vertex predecessors to the vertex itself
-      put(predecessor_map, current_vertex, current_vertex);
+            visitor.finish_vertex(min_vertex, graph);
+        } // end while queue not empty
     }
 
-    // Set distance for start_vertex to zero
-    put(distance_map, start_vertex, distance_zero);
+    // Full init version
+    template <
+        typename Graph, typename PredecessorMap, typename DistanceMap,
+        typename WeightMap, typename VertexIndexMap, typename DistanceCompare,
+        typename DistanceWeightCombine, typename DistanceInfinity,
+        typename DistanceZero, typename DijkstraVisitor
+    >
+    void dijkstra_shortest_paths_no_color_map(
+        const Graph& graph,
+        typename graph_traits<Graph>::vertex_descriptor start_vertex,
+        PredecessorMap predecessor_map, DistanceMap distance_map,
+        WeightMap weight_map, VertexIndexMap index_map,
+        DistanceCompare distance_compare,
+        DistanceWeightCombine distance_weight_combine,
+        DistanceInfinity distance_infinity, DistanceZero distance_zero,
+        DijkstraVisitor visitor,
+        typename boost::disable_if<
+            parameter::are_tagged_arguments<
+                PredecessorMap, DistanceMap, WeightMap, VertexIndexMap,
+                DistanceCompare, DistanceWeightCombine, DistanceInfinity,
+                DistanceZero, DijkstraVisitor
+            >,
+            mpl::true_
+        >::type = mpl::true_()
+    )
+    {
+        // Initialize vertices
+        BGL_FORALL_VERTICES_T(current_vertex, graph, Graph)
+        {
+            visitor.initialize_vertex(current_vertex, graph);
 
-    // Pass everything on to the no_init version
-    dijkstra_shortest_paths_no_color_map_no_init(graph,
-      start_vertex, predecessor_map, distance_map, weight_map,
-      index_map, distance_compare, distance_weight_combine,
-      distance_infinity, distance_zero, visitor);
-  }
-}} // namespace boost::graph
+            // Default all distances to infinity
+            put(distance_map, current_vertex, distance_infinity);
 
-namespace boost { namespace graph {
+            // Default all vertex predecessors to the vertex itself
+            put(predecessor_map, current_vertex, current_vertex);
+        }
+
+        // Set distance for start_vertex to zero
+        put(distance_map, start_vertex, distance_zero);
+
+        // Pass everything on to the no_init version
+        dijkstra_shortest_paths_no_color_map_no_init(
+            graph, start_vertex, predecessor_map, distance_map, weight_map,
+            index_map, distance_compare, distance_weight_combine,
+            distance_infinity, distance_zero, visitor
+        );
+    }
 
     template <typename Graph, typename Args>
     void dijkstra_shortest_paths_no_color_map(
